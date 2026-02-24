11a4db9a8a7209da9dd8678a9d81940e607f2bba
NicholasJDi <NicholasJDi@outlook.com>
2026-02-24 17:27:52 -0600

    add sidebar url param

--- stellara/data/sidebar.js
+++ stellara/data/sidebar.js
@@ -5,10 +5,33 @@ document.addEventListener('DOMContentLoaded', ()=>{
 	// Prevent animation on initial load
 	sidebar.classList.add('no-anim');
 
-	// Restore saved state (if any)
-	const saved = localStorage.getItem('sidebar-open');
-	if (saved !== 'false') {
-		sidebar.classList.add('open');
+
+	const queryString = window.location.search;
+	const searchParams = new URLSearchParams(queryString);
+	const sidebarParam = searchParams.get('sidebar');
+
+	if (sidebarParam === null) {
+		// Restore saved state (if any)
+		const saved = localStorage.getItem('sidebar-open');
+		if (saved !== 'false') {
+			sidebar.classList.add('open');
+		} else {
+			sidebar.classList.remove('open');
+		}
+	} else if (sidebarParam === 'open') {
+		// Force the sidebar to be open
+		if (document.documentElement.clientWidth <= 800) {
+			sidebar.classList.add('open');
+		} else {
+			sidebar.classList.remove('open');
+		}
+	} else {
+		// Force the sidebar to be closed
+		if (document.documentElement.clientWidth <= 800) {
+			sidebar.classList.remove('open');
+		} else {
+			sidebar.classList.add('open');
+		}
 	}
 
 	// Allow animations after initial state is applied
