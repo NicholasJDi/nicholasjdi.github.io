696227a69487070b879134d5f54521e1043a9c59
NicholasJDi <NicholasJDi@outlook.com>
2026-03-21 18:12:00 -0500

    this could fix it fully but idk
    it's annoying that i have to commit to test this

--- stellara/data/sidebar.js
+++ stellara/data/sidebar.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', ()=>{
 	const sidebarParam = searchParams.get('sidebar');
 	let isOpen = true;
 	
-	// Prevent animation on initial load (fallback)
+	// Prevent animation on initial load
 	sidebar.classList.add('no-anim');
 	if (!sidebarParam) {
 		// Restore saved state (if any)
